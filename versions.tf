0ff48584550f462b40b56e177745443a35fdb56c
Sebastián Olaya <solayap@unal.edu.co>
2025-10-16 23:51:15 -0500

    Complete migration: backend now points to new bucket

--- versions.tf
+++ versions.tf
@@ -2,7 +2,7 @@ terraform {
   required_version = ">= 1.5.7"
 
   backend "s3" {
-    bucket = "cmtr-g40pyt8i-backend-bucket-1760673118"
+    bucket = "cmtr-g40pyt8i-backend-new-bucket-1760673118"
     key    = "tf_code.tfstate"
     region = "us-east-1"
   }
