de65165c8b8cfb7438001a57a4d4b861972e5082
Sebastián Olaya <solayap@unal.edu.co>
2025-10-16 23:18:49 -0500

    Remove backend configuration - start with local backend

--- versions.tf
+++ versions.tf
@@ -1,12 +1,6 @@
 terraform {
   required_version = ">= 1.5.7"
 
-  backend "s3" {
-    bucket = "cmtr-g40pyt8i-backend-bucket-1760673118"
-    key    = "tf_code.tfstate"
-    region = "us-east-1"
-  }
-
   required_providers {
     aws = {
       source  = "hashicorp/aws"
